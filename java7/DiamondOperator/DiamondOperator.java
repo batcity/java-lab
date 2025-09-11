515d1674b457cb6af2fc8f9256dda2295dc0d83b
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2025-09-12 03:53:54 +0530

    Add implemention for Diamond operator

---
+++ java7/DiamondOperator/DiamondOperator.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class DiamondOperator {
+
+    public static void main(String args[]) {
+
+        // Diamond operator is used here on the RHS
+        ArrayList<String> testList = new ArrayList<>();
+        testList.add("Test");
+        System.out.println(testList);
+    }
+}
---
+++ java7/DiamondOperator/README.md
@@ -0,0 +1,7 @@
+# DiamondOperator in Java 7
+
+## Concept
+The diamond operator is just a shorthand that lets the compiler infer the generic type from context, saving you from repeating yourself.
+
+## Example
+See [DiamondOperator.java](./DiamondOperator.java)
