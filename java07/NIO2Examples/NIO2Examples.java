bb4a77f333a6f522564cb85128fe0ce5a73ae9ec
Gautham Hullikunte <46633282+batcity@users.noreply.github.com>
2026-06-07 18:53:24 +0530

    start work on nio2 example

--- java07/NIO2Examples/NIO2Examples.java
+++ java07/NIO2Examples/NIO2Examples.java
@@ -1,6 +1,11 @@
-import java.nio.file;
+package com.github.batcity.java.lab.nio2;
+import java.nio.file.Path;
 
+// use this tutorial to write this example btw:
+// currently here -> https://docs.oracle.com/javase/8/docs/api/java/nio/file/Path.html
 public class Nio2Examples {
+
+    public static void main(String args[]) {}
     
 
 }
